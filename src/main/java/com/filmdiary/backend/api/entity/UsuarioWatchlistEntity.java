86eeb565d2291e325482fa1b9f1af6c536547d4c
Juan Blanco Moyano <juablancomoyano@gmail.com>
2025-06-05 09:18:32 +0200

    Añadidas relaciones a UsuarioWatchlistEntity

--- src/main/java/com/filmdiary/backend/api/entity/UsuarioWatchlistEntity.java
+++ src/main/java/com/filmdiary/backend/api/entity/UsuarioWatchlistEntity.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,5 +29,13 @@ public class UsuarioWatchlistEntity {
 
     @Column(name = "fecha_agregado")
     private LocalDateTime fechaAgregado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pelicula_id", insertable = false, updatable = false)
+    private PeliculaEntity pelicula;
+
+    @ManyToOne(fetch = FetchType.LAZY) 
+    @JoinColumn(name = "usuario_id", insertable = false, updatable = false)
+    private UsuarioEntity usuario;
 
 }
